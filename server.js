4c902f89d481229f50039a1ceca85e26f57962d4
Pierre IFOCOP <pierre@ifo.cop>
2018-02-21 12:20:19 +0100

    heroku deploy

--- server.js
+++ server.js
@@ -2,7 +2,19 @@ const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 5000;
-
+const cors = require('cors');
+app.use(bodyParser.json())
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname));
+app.use(cors({ origin: 'null', credentials: true }));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", 'http://localhost:3000');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 app.use(express.static('client/build'));
 
 app.get('/api/', (req, res) => {
