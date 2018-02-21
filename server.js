c095a6b5edff91a4c67da6421e202839070fad1a
Pierre IFOCOP <pierre@ifo.cop>
2018-02-21 12:43:36 +0100

    disabling cors

--- server.js
+++ server.js
@@ -2,17 +2,9 @@ const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 5000;
-const cors = require('cors');
 
 app.use(express.static(__dirname));
-app.use(cors({ origin: 'null', credentials: true }));
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+
 app.use(express.static('client/build'));
 
 app.get('/api/', (req, res) => {
