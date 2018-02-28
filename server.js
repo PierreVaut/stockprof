26d00a4669b93c8692c4470ec1177ba288360e7a
Pierre IFOCOP <pierre@ifo.cop>
2018-02-28 16:06:41 +0100

    WebSocket connection to CEX.io

--- server.js
+++ server.js
@@ -4,15 +4,14 @@ const port = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
 const server = app.listen(port);
 const io = require('socket.io')(server);
+const WebSocket = require('ws');
 console.log(`[Server] Node and Socket.io : listening on port ${port}`)
 import { cookie } from './custom_modules/cookie-handler';
 import { db } from './custom_modules/db-handler';
 import { session } from './custom_modules/session-handler';
 import { args } from './config/connect';
 
-// Connect to CEX.io ws-APi
-const WebSocket = require('ws');
-const cexWS = new WebSocket('wss://ws.cex.io/ws/', { perMessageDeflate: false });
+
 
 db.init();
 
@@ -106,29 +105,20 @@ app.get('/api/', (req, res) => {
 */
 
 let arrayMsg = [];
-io.on('connection', function(client){
+
+const cexioWS = function(client){
+    // Connect to CEX.io ws-APi
     
-    console.log('[Socket.io] Connected');
-    client.on('subscribeToTimer', (interval) => {
-        console.log('[Socket.io] Client is subscribing to timer with interval ', interval);
-        setInterval(() => {
-            client.emit('timer', new Date());
-        }, interval);
-    });
-
-    client.on('chatMessage', (msg) => {
-        console.log('[Socket.io] receiving Msg: ', msg);
-        arrayMsg.push(msg);
-        client.emit('arrayMessage', arrayMsg)
-    });
-
+    const cexWS = new WebSocket('wss://ws.cex.io/ws/', { perMessageDeflate: false });
+    console.log('[cexioWS] - starting'); 
     cexWS.on('open', function(){
+        console.log('[cexioWS] - open'); 
         cexWS.on('message', function(el){
             console.log('[CEX server] message:', el)
             let msg = JSON.parse(el);
-    
-            client.emit('btc', msg);
-    
+            if(client){
+                client.emit('btc', msg);
+            }
             if(msg['e'] === 'ping'){
                 console.log('[CEX client] Connection active')
                 cexWS.send(JSON.stringify({"e":"pong"}));
@@ -156,42 +146,30 @@ io.on('connection', function(client){
         )
     });
 
-});
+}
 
-
-cexWS.on('open', function(){
-    cexWS.on('message', function(el){
-        console.log('[CEX server2] message:', el)
-        let msg = JSON.parse(el);
-
-        //client.emit('btc', msg);
-
-        if(msg['e'] === 'ping'){
-            console.log('[CEX client2] Connection active')
-            cexWS.send(JSON.stringify({"e":"pong"}));
-        }
-
-        if(msg.data){
-            if((msg.data.symbol1 === "BTC" && msg.data.symbol2 === "EUR") ){
-                console.log('[CEX server2] BTC: ', msg.data)
-                let temp = msg.data;
-
-            }
-        }
+io.on('connection', function(client){
+    
+    console.log('[Socket.io] Connected');
+    client.on('subscribeToTimer', (interval) => {
+        console.log('[Socket.io] Client is subscribing to timer with interval ', interval);
+        setInterval(() => {
+            client.emit('timer', new Date());
+        }, interval);
     });
 
+    client.on('chatMessage', (msg) => {
+        console.log('[Socket.io] receiving Msg: ', msg);
+        arrayMsg.push(msg);
+        client.emit('arrayMessage', arrayMsg)
+    });
 
-    cexWS.on('open',    (el) => console.log('[CEX.io2] open: ',el) );
-    cexWS.on('error',   (el) => console.log('[CEX.io2] error: ',el) );
-    cexWS.on('close',   (el) => console.log('[CEX.io2] close: ',el) );
-    cexWS.send( JSON.stringify(args) );
+    cexioWS(client);
 
-    cexWS.send(JSON.stringify({
-        e: 'subscribe',
-        rooms: ['tickers']
-        })
-    )
 });
+
+
+
 
 
 
