67907389fe34a39880225f84f2cbb36a6b0561f4
Pierre IFOCOP <pierre@ifo.cop>
2018-03-01 14:58:28 +0100

    Redux implement

--- client/src/index.js
+++ client/src/index.js
@@ -6,10 +6,19 @@ import {unregister} from './registerServiceWorker';
 // import registerServiceWorker from './registerServiceWorker';
 
 import { CookiesProvider } from 'react-cookie';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './components/App'
+import reducer from './reducers'
+
+
+const store = createStore(reducer);
 
 ReactDOM.render(
     <CookiesProvider>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </CookiesProvider>
     , document.getElementById('root'));
 
