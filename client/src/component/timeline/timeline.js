10b5637812d8ce2c921e3fdfe9dc0903ce65830c
Carburator 11 <p.vautherin@hotmail.com>
2018-07-13 11:49:46 +0200

    [Added] Timeline : Search function + filter friends-only

--- client/src/component/timeline/timeline.js
+++ client/src/component/timeline/timeline.js
@@ -7,25 +7,62 @@ import {
   updateTimelineItem as updateTimelineItemAC } from '../../actions/';
 
 class Timeline extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: '',
+      viewFriendsOnly: false,
+    };
+  }
+
   componentDidMount() {
     this.props.getTimeline();
   }
 
   render() {
-    const { session, handleSubmit, timeline } = this.props;
+    const {
+      session, handleSubmit, timeline, account,
+    } = this.props;
+    const { friends } = account;
+    const { search, viewFriendsOnly } = this.state;
+
     return (
       <div className="user">
         { session.isLogged ?
-           (timeline ? timeline.map((el, index) =>
-             (<TimelineItem
-               {...el}
-               handleSubmit={handleSubmit}
-               key={index}
-             />)) : <Loader />)
-             :
 
-             <GuestMenu />
-          }
+          (timeline ?
+            <div>
+              <input
+                className="input-text"
+                type="text"
+                placeholder="Rechercher..."
+                onChange={e => { this.setState({ search: e.target.value }); }}
+                value={search}
+              /><br />
+              <input
+                className="input-checkbox"
+                type="checkbox"
+                checked={viewFriendsOnly}
+                onClick={() => { this.setState({ viewFriendsOnly: !viewFriendsOnly }); }}
+              />
+              <span className="input-checkbox-text"> Afficher uniquement les amis</span> <br /><br />
+              {timeline.filter(timelineItem => {
+                if (!viewFriendsOnly) {
+                  return timelineItem.author.toLowerCase().includes(search.toLowerCase());
+                }
+                return (
+                  timelineItem.author.toLowerCase().includes(search.toLowerCase())
+                  && friends.includes(timelineItem.authorId)
+                );
+              }).map((el, index) =>
+                (<TimelineItem
+                  {...el}
+                  handleSubmit={handleSubmit}
+                  key={index}
+                />))}
+            </div>
+          : <Loader />)
+        : <GuestMenu />}
       </div>
     );
   }
