ce45d5fc12d0866fdd7a99d8cd2e9ef2effa7156
Carburator 11 <p.vautherin@hotmail.com>
2018-07-18 09:59:49 +0200

    Socket and Chat 'rather' OK

--- client/src/component/menu.js
+++ client/src/component/menu.js
@@ -37,11 +37,13 @@ class Menu extends React.Component {
       }
     });
     socket.emit('btc-initial', 'hello');
+    /*
     const { _id } = this.props.account;
+
     if (_id) {
       console.log('Emit notification', _id);
       socket.emit('notification', _id);
-    }
+    } */
   }
 
   render() {
