8f45227873247606aa42447eb3dc6b1a7ce2e66f
Carburator 11 <p.vautherin@hotmail.com>
2018-07-18 11:07:54 +0200

    [IN PROGRESS] Deactivated WebSockets to prevent Crashes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import { port } from './api/routes';
 // Database
 import { db } from './custom_modules/db-handler';
 import { priceDB } from './custom_modules/price-handler';
-// import { ioServer } from './custom_modules/ws-handler';
+import { ioServer } from './custom_modules/ws-handler';
 
 db.init();
 priceDB.init();
